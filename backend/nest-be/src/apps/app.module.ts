3ca3156eec05bc25ba00c8fe9543505cd9b3141e
nirjana <sujatag391@gmail.com>
2024-01-15 00:57:50 +0545

    backend configure

---
+++ backend/nest-be/src/apps/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController, UserController],
+  providers: [AppService],
+})
+export class AppModule {}
